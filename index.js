90432bfde23702d20c06c9fce637b0bbe070e436
Lei Yang <leiyangyou@gmail.com>
2015-09-22 22:55:10 +0800

    remove pm2 dependency

--- index.js
+++ index.js
@@ -30,8 +30,19 @@ router.post('/watermark', function *(next) {
   yield next;
 });
 
-app
-  .use(logger())
-  .use(router.routes())
-  .use(router.allowedMethods())
-  .listen(8088);
+var numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+} else {
+  app
+    .use(logger())
+    .use(router.routes())
+    .use(router.allowedMethods())
+    .listen(8088);
+}
